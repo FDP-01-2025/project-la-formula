7988c1b798cbeec213819198afad10d4f829c372
DavidM09 <00201825@uca.edu.sv>
2025-06-25 11:34:25 -0600

    ss

---
+++ mapa2/mapa2.h
@@ -0,0 +1,51 @@
+#ifndef MAPA_H2
+#define MAPA_H2
+
+const int MAP_WIDTH2 = 80;    //ancho de mapa
+const int MAP_HEIGHT2 = 20;   //alto de mapa
+const int VIEW_WIDTH2 = 50;   //vista de ancho de mapa
+const int VIEW_HEIGHT2 = 20;  //vista de alto de mapa
+
+char mapa2 [MAP_WIDTH2][MAP_HEIGHT2 + 1] = {
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
